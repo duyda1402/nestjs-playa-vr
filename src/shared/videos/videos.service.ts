624fc571326581153a5cc308edc895956776ab3b
Duy Do <anhduycnttx1@gmail.com>
2023-03-16 01:24:17 +0700

    fix/sort-by-name

--- src/shared/videos/videos.service.ts
+++ src/shared/videos/videos.service.ts
@@ -500,9 +500,9 @@ export class VideoService {
   private query27 = `REPLACE(${this.query26}, "–", '')`;
   private query28 = `REPLACE(${this.query27}, ",", '')`;
   private query29 = `REPLACE(${this.query28}, "?", '')`;
-  private query30 = `REPLACE(${this.query29}, ";", '')`;
-  private query31 = `REPLACE(${this.query30}, "<", '')`;
-  private query32 = `REPLACE(${this.query31}, ">", '')`;
+  // private query30 = `REPLACE(${this.query29}, ";", '')`;
+  // private query31 = `REPLACE(${this.query30}, "<", '')`;
+  // private query32 = `REPLACE(${this.query31}, ">", '')`;
   // private query33 = `REPLACE(${this.query32}, "/", '')`;
 
   // private query35 = `REPLACE(${this.query32}, "$", '')`;
@@ -510,5 +510,5 @@ export class VideoService {
   // private query37 = `REPLACE(${this.query36}, "*", '')`;
   // private query38 = `REPLACE(${this.query37}, "+", '')`;
   // private query39 = `REPLACE(${this.query38}, "~", '')`;
-  private queryReplace = this.query32;
+  private queryReplace = this.query29;
 }
