1cbc88c066f54d10d297d5131eb80f2c2078cc71
Duy Do <anhduycnttx1@gmail.com>
2023-03-15 22:27:55 +0700

    fix/sort-by-name

--- src/shared/videos/videos.service.ts
+++ src/shared/videos/videos.service.ts
@@ -142,7 +142,7 @@ export class VideoService {
         'post.postTitle as postTitle',
         'IFNULL(pp.ppdate, post.postDate) as `release_date`',
       ])
-      .addSelect(`REPLACE(REPLACE(REPLACE(post.postTitle,'\\\\W /g', ''), '\ /g"',''),"# /g",'')`, 'nametranform');
+      .addSelect(`REPLACE(REPLACE(REPLACE(post.postTitle,'/[^\w\s]/gi', ''), '\" /gi',''),"# /gi",'')`, 'nametranform');
 
     const dataPromis = queryVideo
       .limit(query.perPage)
