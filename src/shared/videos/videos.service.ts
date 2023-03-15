cd083bcd648d2a8540f12ed58b340da106a84942
Duy Do <anhduycnttx1@gmail.com>
2023-03-16 00:39:24 +0700

    fix/sort-by-name

--- src/shared/videos/videos.service.ts
+++ src/shared/videos/videos.service.ts
@@ -471,5 +471,5 @@ export class VideoService {
     return details;
   }
 
-  private queryReplace = `REPLACE(REPLACE(REPLACE(REPLACE(post.postTitle, '\\"', ''),"\\'",""),"#",""),"\\(","")`;
+  private queryReplace = `REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(post.postTitle, '\\"', ''),"\\'",""),"#",""),"\\(",""),".",""),"0",""),"1",""),"2",""),"3",""),"4",""),"5","")`;
 }
