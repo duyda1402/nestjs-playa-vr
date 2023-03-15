f587d8c561ecf7296410eb5132f23be55f2a9319
Duy Do <anhduycnttx1@gmail.com>
2023-03-16 01:29:30 +0700

    fix/sort-by-name

--- src/shared/version/version.service.ts
+++ src/shared/version/version.service.ts
@@ -6,7 +6,7 @@ import { IFSemVersion, IFRsp } from 'src/types/response.type';
 export class VersionService {
   async getVersion(): Promise<IFRsp<IFSemVersion>> {
     // Kiểm tra thông tin version
-    const version = '0.3.1';
+    const version = '1.0.0';
     return { status: { code: 1, message: 'ok' }, data: version };
   }
 }
