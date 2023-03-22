86f80920ec41115e5a7bddb08e72ec53e3a76351
doan <doan@swearl.com>
2023-03-22 22:02:41 +0700

    Update for logging api

--- src/shared/logging/logging.module.ts
+++ src/shared/logging/logging.module.ts
@@ -4,9 +4,10 @@ import { LoggingService } from './logging.service';
 import { EntitiesModule } from 'src/entities/entities.module';
 import {VideoModule} from "../videos/videos.module";
 import {UserModule} from "../user/user.module";
+import {JwtModule} from "@nestjs/jwt";
 
 @Module({
-  imports: [EntitiesModule, VideoModule, UserModule],
+  imports: [EntitiesModule, VideoModule, UserModule, JwtModule.register({})],
   controllers: [LoggingController],
   providers: [LoggingService],
 })
