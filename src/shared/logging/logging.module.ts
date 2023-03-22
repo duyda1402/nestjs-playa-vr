955170c4288a91f235a18ba64b1267008add6569
doan <doan@swearl.com>
2023-03-22 22:08:37 +0700

    Update for categories api

--- src/shared/logging/logging.module.ts
+++ src/shared/logging/logging.module.ts
@@ -6,10 +6,13 @@ import {VideoModule} from "../videos/videos.module";
 import {UserModule} from "../user/user.module";
 import {JwtModule} from "@nestjs/jwt";
 import {VideoService} from "../videos/videos.service";
+import {MyOpensearchModule} from "../open-search/opensearch.module";
+import {CommonModule} from "../common/common.module";
+import {OpenSearchService} from "../open-search/opensearch.service";
 
 @Module({
-  imports: [EntitiesModule, VideoModule, UserModule, JwtModule.register({})],
+  imports: [EntitiesModule, MyOpensearchModule, CommonModule, VideoModule, UserModule, JwtModule.register({})],
   controllers: [LoggingController],
-  providers: [LoggingService, VideoService],
+  providers: [LoggingService, OpenSearchService, VideoService],
 })
 export class LoggingModule {}
