59a889be182209f1c24410553fb27d75bfc8157f
Duy Do <anhduycnttx1@gmail.com>
2023-03-25 15:37:20 +0700

    fix/studio

--- src/shared/studios/studios.service.ts
+++ src/shared/studios/studios.service.ts
@@ -74,7 +74,7 @@ export class StudiosService {
           .leftJoin(TermRelationShipsBasicEntity, 'termRelationPost', 'postForStudio.id = termRelationPost.objectId')
           .andWhere('termRelationPost.termId = :termPostId', { termPostId: 251 });
       }, 'totalPost')
-      .having('total > 0');
+      .andWhere('totalPost > 0');
     const dataPromise = studioQuery
       .limit(query.perPage)
       .orderBy(order, direction)
