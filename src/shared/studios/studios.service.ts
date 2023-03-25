14da62f141acdde5eb9c7ab9f23e484dc98ce249
Duy Do <anhduycnttx1@gmail.com>
2023-03-25 17:36:20 +0700

    fix/studio

--- src/shared/studios/studios.service.ts
+++ src/shared/studios/studios.service.ts
@@ -62,28 +62,26 @@ export class StudiosService {
           .where('tr.termId = term.id');
       }, 'popularity');
     }
-    studioQuery
-      .addSelect((subQuery) => {
-        return subQuery
-          .select('COUNT(postForStudio.id)', 'total')
-          .from(PostEntity, 'postForStudio')
-          .where('postForStudio.postType = :postType', { postType: 'post' })
-          .andWhere('postForStudio.postStatus = :postStatus', { postStatus: 'publish' })
-          .leftJoin(TermRelationShipsBasicEntity, 'trStudioPost', 'trStudioPost.objectId = postForStudio.id')
-          .andWhere('trStudioPost.termId = term.id')
-          .leftJoin(TermRelationShipsBasicEntity, 'termRelationPost', 'postForStudio.id = termRelationPost.objectId')
-          .andWhere('termRelationPost.termId = :termPostId', { termPostId: 251 })
-          .andWhere((qb) => {
-            const subQuery = qb
-              .subQuery()
-              .select('termPostExist.objectId')
-              .from(TermRelationShipsBasicEntity, 'termPostExist')
-              .where(`termPostExist.termId IN (:...termIds)`, { termIds: [4244, 5685] })
-              .getQuery();
-            return `postForStudio.id NOT IN (${subQuery})`;
-          });
-      }, 'total')
-      .having('total > 0');
+    studioQuery.andHaving('total > 0').addSelect((subQuery) => {
+      return subQuery
+        .select('COUNT(postForStudio.id)', 'total')
+        .from(PostEntity, 'postForStudio')
+        .where('postForStudio.postType = :postType', { postType: 'post' })
+        .andWhere('postForStudio.postStatus = :postStatus', { postStatus: 'publish' })
+        .leftJoin(TermRelationShipsBasicEntity, 'trStudioPost', 'trStudioPost.objectId = postForStudio.id')
+        .andWhere('trStudioPost.termId = term.id')
+        .leftJoin(TermRelationShipsBasicEntity, 'termRelationPost', 'postForStudio.id = termRelationPost.objectId')
+        .andWhere('termRelationPost.termId = :termPostId', { termPostId: 251 })
+        .andWhere((qb) => {
+          const subQuery = qb
+            .subQuery()
+            .select('termPostExist.objectId')
+            .from(TermRelationShipsBasicEntity, 'termPostExist')
+            .where(`termPostExist.termId IN (:...termIds)`, { termIds: [4244, 5685] })
+            .getQuery();
+          return `postForStudio.id NOT IN (${subQuery})`;
+        });
+    }, 'total');
     // .groupBy('term.id')
 
     const dataPromise = studioQuery
